bea25f9a2125aa1f5aa1cda6869e9b527fcb53e1
Alfredo ws <sick_freedylp@hotmail.com>
2021-04-25 00:27:18 -0500

    first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+
+//static files
+
+app.use(express.static(__dirname + '/build'))
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/build/index.html'); 
+})	
+
+
+app.listen(8000, (err)=>{
+	if(err){
+		console.log('error initializing server ')
+	}else{
+		console.log('server running on http://localhost:8000');
+	}
+});
